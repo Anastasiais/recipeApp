e18b57f3794ce78355c750419664ac3a71ba91de
Anastasiais <anastasiasedova2013@yandex.ru>
2021-06-15 21:22:01 +0300

    update styles of button

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Recipe from './Recipe'
 import './App.css';
 
 const App = () => {
-  
+
   const APP_ID = "022c4001"
   const APP_KEY = "895297ab7bed778d51ea1a5831b838f9"
 
@@ -16,16 +16,16 @@ const App = () => {
       const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
       const data = await response.json()
       setRecipes(data.hits)
-      
-      
-   } 
-   getRecipes() 
+
+
+   }
+   getRecipes()
   }, [query])
 
 
 const updateSearch = e => {
    setSearch(e.target.value)
-  
+
 }
 
 const getSearch = e => {
@@ -42,14 +42,14 @@ const getSearch = e => {
       </form>
         <div  className="recipes">
       {recipes.map((recipe, index)=> (
-      <Recipe 
+      <Recipe
         key={index}
-        title={recipe.recipe.label} 
+        title={recipe.recipe.label}
         calories={recipe.recipe.calories}
         image={recipe.recipe.image}
         ingredients={recipe.recipe.ingredients}
       />
-      ))} 
+      ))}
       </div>
     </div>
   );
